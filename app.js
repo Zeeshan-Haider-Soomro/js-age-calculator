88833cb610a14f85c59244d0523edd20494b85a7
Zeeshan-Haider-Soomro <159009736+Zeeshan-Haider-Soomro@users.noreply.github.com>
2024-05-21 00:04:04 +0500

    done

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+
+let date = new Date()
+let yearChanger = document.querySelector("#yearChanger");
+yearChanger.innerText = date.getFullYear()
+
 function ageCalculate(){
     let currentYear = 2024
     let year = +prompt("Enter your date of birth");
